c2cc76ae23aa26dbd8f6917bf614af981f6552ab
Aniket <Aniket8076206264@gmail.com>
2024-01-26 15:27:12 +0530

    card added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/nav';
 import ImageContainer from './components/Imagecontainer/op.jsx';
+import Homepage from './components/Homepage/Card.jsx';
+
 
 function App() {
   return (
     <div>
   <Navbar></Navbar>
 <ImageContainer></ImageContainer>
+<Homepage></Homepage>
     </div>
   );
 }
