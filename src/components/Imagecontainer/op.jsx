730b54c6c74efb04d878859374b29db6180d9d3d
Aniket <Aniket8076206264@gmail.com>
2024-01-26 14:34:09 +0530

    button fix

--- src/components/Imagecontainer/op.jsx
+++ src/components/Imagecontainer/op.jsx
@@ -8,15 +8,15 @@ import X from "../Assets/x.png";
 const ImageContainer = () => {
   return (
     <div className="image-container">
-      <div className="image-box">
+      <button className="image-box">
         <img src={Y} alt="YOUTUBE" />
-      </div>
-      <div className="image-box">
+      </button>
+      <button className="image-box">
         <img src={I} alt="INSTAGRAM" />
-      </div>
-      <div className="image-box">
+      </button>
+      <button className="image-box">
         <img src={X} alt="TWITTER" />
-      </div>
+      </button>
     </div>
   );
 };
